d5e5c24b644b68d358e44da6245cc589537f8d56
vadimit <vl.orlov@mail.ru>
2014-08-13 21:39:43 +0300

    Edit production.rb for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,7 @@ SimpleApp::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+
+  config.serve_static_assets = true
+  
 end
